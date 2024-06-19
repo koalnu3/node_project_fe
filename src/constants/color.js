42eabdbd3ac20a539530f732c6f99fedcdc76484
minimi61 <new1573@naver.com>
2024-06-20 02:49:52 +0900

    feat: 초기 세팅, react-route-dom설치, 메인컬러 세팅

---
+++ src/constants/color.js
@@ -0,0 +1,4 @@
+export const mainRed = "#D60036";
+export const black = "#000000";
+export const white = "#ffffff";
+export const mainGray = "#55565A";
---
+++ src/page/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+
+const LoginPage = () => {
+  return <div>로그인</div>;
+};
+
+export default LoginPage;
