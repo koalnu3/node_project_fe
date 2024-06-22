068856db25d2555d8e5dbe761ff79f50df33ea22
minimi61 <new1573@naver.com>
2024-06-23 02:58:08 +0900

    [Feat] 토스트메시지 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 }
 
 export default App;
