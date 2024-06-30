d1862b9db3329cf81b9ae5c5e7eb7240cc6eac97
Seam Park <run128@naver.com>
2024-06-30 19:20:32 +0900

    어드민 레이아웃 정리

--- src/page/AdminPage.js
+++ src/page/AdminPage.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import Tab from '../components/Tab';
-import HandleUser from '../components/AdminComponents/HandleUser';
-import HandleTeacher from '../components/AdminComponents/HandleTeacher';
-import "../style/AdminPage.style.css"
-import useAdminPageStore from '../store/useAdminPageStore';
+import React, { useEffect, useState } from "react";
+import Tab from "../components/Tab";
+import HandleUser from "../components/AdminComponents/HandleUser";
+import HandleTeacher from "../components/AdminComponents/HandleTeacher";
+import "../style/AdminPage.style.css";
+import useAdminPageStore from "../store/useAdminPageStore";
+import Content from "../components/Content";
 const AdminPage = () => {
   const [tabActive, setTabActive] = useState("");
- 
+
   const tabList = [
-    { name: '회원관리', link: '#handleUser' },
-    { name: '강사관리', link: '#handleTeacher' },
+    { name: "회원관리", link: "#handleUser" },
+    { name: "강사관리", link: "#handleTeacher" },
   ];
 
   const {
@@ -17,41 +18,42 @@ const AdminPage = () => {
     setInputName,
     setNickName,
     setSelectedUser,
-    setSelectedUserId
-  } = useAdminPageStore()
+    setSelectedUserId,
+  } = useAdminPageStore();
 
   useEffect(() => {
-    if (tabActive === '회원관리') {
-      setLevel('customer');
-      setInputName("")
-      setNickName("")
-      setSelectedUserId("")
-      setSelectedUser(null)
-    } else if (tabActive === '강사관리') {
-      setLevel('teacher');
-      setInputName("")
-      setNickName("")
-      setSelectedUserId("")
-      setSelectedUser(null)
+    if (tabActive === "회원관리") {
+      setLevel("customer");
+      setInputName("");
+      setNickName("");
+      setSelectedUserId("");
+      setSelectedUser(null);
+    } else if (tabActive === "강사관리") {
+      setLevel("teacher");
+      setInputName("");
+      setNickName("");
+      setSelectedUserId("");
+      setSelectedUser(null);
     }
   }, [tabActive, setLevel]);
 
   return (
-      <div className='admin-content'>
-
-        <div className='admin-page-main'>
-    <Tab 
-          list={tabList}
-          tabActive={tabActive}
-          setTabActive={setTabActive}
-          tagType="a"
-          preventDefault={true}
-        />
+    <Content className="adminPage">
+      <div className="admin-content">
+        <div className="admin-page-main">
+          <Tab
+            list={tabList}
+            tabActive={tabActive}
+            setTabActive={setTabActive}
+            tagType="a"
+            preventDefault={true}
+          />
         </div>
-      
-        {tabActive == '회원관리' ? <HandleUser/> : <HandleTeacher/>}
+
+        {tabActive === "회원관리" ? <HandleUser /> : <HandleTeacher />}
       </div>
+    </Content>
   );
-}
+};
 
 export default AdminPage;
